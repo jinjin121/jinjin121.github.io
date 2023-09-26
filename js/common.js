2bc88d25eb82d750fb732106b0ae9fd5b9c39397
jinjin121 <110872022+jinjin121@users.noreply.github.com>
2023-09-26 18:18:51 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -24,6 +24,31 @@ $(function () {
         },
 
     }
+    var slide = new Swiper('.swiper', {
+        slidesPerView: '1', // 한 슬라이드에 보여줄 갯수
+        loop: true, // 슬라이드 반복 여부
+        pagination: true, // pager 여부
+        autoplay: { // 자동 슬라이드 설정 
+            delay: 5000, // 시간 설정
+            disableOnInteraction: false, // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
+        },
+        navigation: { // 버튼 사용자 지정
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        //breakpoints: { //반응형
+        // 화면의 넓이가 320px 이상일 때
+        //320: {
+        //slidesPerView: 2,
+        //spaceBetween: 20
+        //},
+        // 화면의 넓이가 640px 이상일 때
+        //640: {
+        //slidesPerView: 4,
+        //spaceBetween: 40
+        // }
+        //}
+    })
     $('#main_full').fullpage(option);
 
     $('.grapic_btn li').on('click', function (e) {
@@ -32,6 +57,5 @@ $(function () {
         $(this).addClass('on').siblings().removeClass('on');
         $('.grapic').eq(idx).addClass('on').siblings().removeClass('on');
     });
-
 
 })
